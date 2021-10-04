d5bbbabd96881a29845fb6f7d1e5560a1088ce0f
Carlo <26264+carlotrimarchi@users.noreply.github.com>
2021-10-04 12:52:22 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+/*
+Example of adding event listenrs to radio buttons
+
+let statusFilters = document.querySelectorAll("input[name=status]");
+
+for (let statusFilter of statusFilters) {
+  statusFilter.addEventListener("change", (e) => {
+    console.log(e.target.value);
+  });
+}
+*/
